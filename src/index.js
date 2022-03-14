c639ec8fd50a4a6a0502917529c745ad4c724259
stavponte27 <stavponte27@gmail.com>
2022-03-14 13:38:28 +0200

    Fix(): Fix export and change index.ts to index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as parser from './utils/parseColumn.util';
+import { DataSources as SupportedDataSources } from '../common/enum.common';
+import * as types from '../common/types/type.common';
+
+export { parser, types, SupportedDataSources };
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import * as Parser from './utils/parseColumn.util';
-
-export { Parser };
